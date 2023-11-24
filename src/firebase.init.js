5821a793512bb507ac992880496b961023b77ff8
Vikashnautiyal224 <vikashnautiyal224@gmail.com>
2023-11-24 22:03:52 +0530

    Loginpage

---
+++ src/firebase.init.js
@@ -0,0 +1,20 @@
+
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import {getAuth} from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCWamE-g5sLkH6q7ap5v7vXwJDHXyOWfUQ",
+  authDomain: "twitter-c388b.firebaseapp.com",
+  projectId: "twitter-c388b",
+  storageBucket: "twitter-c388b.appspot.com",
+  messagingSenderId: "501672457393",
+  appId: "1:501672457393:web:16578b067631641dda5e90",
+  measurementId: "G-YQ103YS4HL"
+};
+
+
+export const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const auth = getAuth(app);
+export default auth;
---
+++ src/pages/Login.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import twitterImage from "../assets/twitter.png";
+const Login = () => {
+  return (
+    <div className="login-container">
+      <div className="image-container">
+        <img src={twitterImage} alt="" />
+      </div>
+      <div className="form-container">
+        
+      </div>
+    </div>
+  );
+};
+export default Login;
