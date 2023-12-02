dec980d351b55d90de703c07c0012e8c74e3e081
Vikashnautiyal224 <vikashnautiyal224@gmail.com>
2023-12-02 15:31:05 +0530

    connecting front end to backend

--- src/pages/Feed/Tweetbox/TweetBox.jsx
+++ src/pages/Feed/Tweetbox/TweetBox.jsx
@@ -5,12 +5,35 @@ import "./TweetBox.css";
 
 export const TweetBox = () => {
   const [post, setPost] = useState("");
-  const [imageURL, setImageURL] = useState("");
+  const [imageURL, setImageURL] = useState(null);
 
   const handleTweet = (e) => {
     e.preventDefault();
-    console.log(post);
+     const userPost ={
+      post:post,
+      photo:imageURL
+     }
+    //  console.log(userPost)
+    fetch(`http://localhost:5000/post`, {
+      method: "POST",
+      headers:{
+       'content-type':'application/json'
+      },
+      body:JSON.stringify(userPost)
+     
+    })
+      .then(res => res.json())
+      .then(data =>{
+        console.log(data);
+      })
   };
+  
+
+  // const handleImageChange = (e) => {
+  //   const file = e.target.files[0];
+  //   setImage(file);
+  // };
+
   return (
     <div className="tweetBox">
       <form onSubmit={handleTweet}>
@@ -18,7 +41,7 @@ export const TweetBox = () => {
           <Avatar src="https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_960_720.png" />
           <input
             type="text"
-            placeholder="What's hapening"
+            placeholder="What's happening"
             onChange={(e) => setPost(e.target.value)}
           />
         </div>
@@ -26,7 +49,12 @@ export const TweetBox = () => {
           <label htmlFor="image" className="imageIcon">
             <AddPhotoAlternateIcon />
           </label>
-          <input type="file" id="image" className="imageInput" />
+          <input
+            type="file"
+            id="image"
+            className="imageInput"
+            // onChange={handleImageChange}
+          />
           <Button className="tweetBox_tweetButton" type="submit">
             Tweet
           </Button>
